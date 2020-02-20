7a402fca71fe1865e4259c74c9919e737063dd46
rouan.thompson <rouan.thompson@gmail.com>
2020-02-20 03:57:33 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,7 +11,6 @@ def input_to_index(input)
   index = input.to_i - 1
 end
 
-board = [" ", " ", " "]
-def move(array, index, player = "X")
-  array[index] = player
+def move(board, index, player = "X")
+  board[index] = player
 end
